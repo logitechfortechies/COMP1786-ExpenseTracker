d6004ee7522be389bf506912960b30c1c42e162b
long <le.long.leader@gmail.com>
2026-04-18 17:41:31 +0700

    Fix base URL

--- app/src/main/java/com/example/expensetracker/network/ApiClient.java
+++ app/src/main/java/com/example/expensetracker/network/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
-    private static final String BASE_URL = "http://10.0.2.2:3000/api/";
+    private static final String BASE_URL = "http://10.0.2.2:3000/";
     private static Retrofit retrofit = null;
 
     public static ApiService getService() {
